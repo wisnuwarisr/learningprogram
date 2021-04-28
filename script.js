44ba05d2bd32c40e83e10641a20bb53d15d57449
wisnuwarisr <78094622+wisnuwarisr@users.noreply.github.com>
2021-04-28 18:36:45 +0700

    first commit

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $("#navbar").addClass("sticky");
     } else {
-      $(".navbar").removeClass("sticky");
+      $("#navbar").removeClass("sticky");
     }
 
     // scroll-up button show/hide script
